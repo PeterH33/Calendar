762f3b36c14afeaca73c6d84c1529b648245fea3
Peter Hartnett <peter.hartnett@gmail.com>
2022-08-31 15:20:38 -0400

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Peter Hartnett on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
